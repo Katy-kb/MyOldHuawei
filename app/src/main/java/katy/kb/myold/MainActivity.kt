1a5c7419d48dc00232e2004cd7f5a51a43b9ecb1
unknown <katy-kb@yandex.ru>
2021-11-30 22:29:20 +0300

    SRS5

---
+++ app/src/main/java/katy/kb/myold/MainActivity.kt
@@ -0,0 +1,53 @@
+package katy.kb.myold
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "Ты видел деву на скале\n" +
+                "В одежде белой над волнами")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Когда, бушуя в бурной мгле,\n" +
+                "Играло море с берегами,")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Когда луч молний озарял\n" +
+                "Ее всечасно блеском алым")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "И ветер бился и летал\n" +
+                "С ее летучим покрывалом?")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Прекрасно море в бурной мгле\n" +
+                "И небо в блесках без лазури;")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Но верь мне: дева на скале\n" +
+                "Прекрасней волн, небес и бури.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+}
